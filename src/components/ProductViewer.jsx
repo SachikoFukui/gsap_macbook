2310090cbc844b5743965a89a06c7288c027a1ab
Sachiko <sachikofukui.it@gmail.com>
2026-09-07 16:04:04 +0200

    Finished Product Viewer + Model Switcher

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,17 +3,23 @@ import useMacbookStore from "../store"
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
 import MacbookModel14 from "./models/Macbook-14";
-import StudioLight from "./StudioLights";
+import StudioLight from "./three/StudioLights.jsx";
+
+import ModelSwitcher from "./three/ModelSwitcher";
+import {useMediaQuery} from "react-responsive";
 
 
 const ProductViewer = () => {
     const {color, scale, setColor, setScale} = useMacbookStore();
+
+    const isMobile = useMediaQuery( {query:"(max-width: 1024px)"});
+
     return (
         <section id="product-viewer">
             <h2>Take a closer look.</h2>
 
             <div className="controls">
-                <p className="info">Macbook {scale} in {color}</p>
+                <p className="info">Macbook Pro | Available in 14" & 16" in Space Gray and Dark colors</p>
 
                 <div className="flex-center gap-5 mt-5">
                     <div className="color-control">
@@ -46,10 +52,8 @@ const ProductViewer = () => {
 
             <Canvas id="canvas" camera={{position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
                 <StudioLight/>
-                
-                <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
 
-                <OrbitControls enableZoom={false}/>
+                <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} />
             </Canvas>
 
         </section>
